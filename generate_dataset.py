ce68dfe1d2b28321121e79119589ff9fc89b5d5b
ferhatSF <32377618+ferhatSF@users.noreply.github.com>
2019-04-22 09:37:07 -0700

    Update generate_dataset.py

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,6 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
 def generate_dataset(W_actual=1.5, b_actual=0.5, 
                      npoints=100,
                      x_random=False,x_start=0, x_range=1,
